8a59f3d58d2296da5a91242c9266abed9a885a17
Daniel Bergey <bergey@alum.mit.edu>
2013-12-01 16:15:51 -0500

    Style changes - fewer local helper functions

--- triangle/direct/Main.hs
+++ triangle/direct/Main.hs
@@ -14,74 +14,63 @@ module Main where
 
 import Control.Monad (unless, when)
 import Graphics.Rendering.OpenGL
-import qualified Graphics.UI.GLFW as G
+import qualified Graphics.UI.GLFW as GLFW
 import System.Exit
 import System.IO
 
--- tiny utility functions, in the same spirit as 'maybe' or 'either'
--- makes the code a wee bit cleaner
-bool :: Bool -> a -> a -> a
-bool b falseRes trueRes = if b then trueRes else falseRes
-
-unless' :: Monad m => m Bool -> m () -> m ()
-unless' action falseAction = do
-    b <- action
-    unless b falseAction
-
-maybe' :: Maybe a -> b -> (a -> b) -> b
-maybe' m nothingRes f = case m of
-    Nothing -> nothingRes
-    Just x  -> f x
-    
 -- type ErrorCallback = Error -> String -> IO ()
-errorCallback :: G.ErrorCallback
-errorCallback err description = hPutStrLn stderr description
+errorCallback :: GLFW.ErrorCallback
+errorCallback _ = hPutStrLn stderr
 
-keyCallback :: G.KeyCallback
-keyCallback window key scancode action mods = when (key == G.Key'Escape && action == G.KeyState'Pressed) $
-  G.setWindowShouldClose window True
+keyCallback :: GLFW.KeyCallback
+keyCallback window key _ action _ = when (key == GLFW.Key'Escape && action == GLFW.KeyState'Pressed) $
+  GLFW.setWindowShouldClose window True
 
 main :: IO ()
 main = do
-  G.setErrorCallback (Just errorCallback)
-  successfulInit <- G.init
+  GLFW.setErrorCallback (Just errorCallback)
+  successfulInit <- GLFW.init
   -- if init failed, we exit the program
-  bool successfulInit exitFailure $ do
-      mw <- G.createWindow 640 480 "Simple example, haskell style" Nothing Nothing
-      maybe' mw (G.terminate >> exitFailure) $ \window -> do
-          G.makeContextCurrent mw
-          G.setKeyCallback window (Just keyCallback)
-          mainLoop window
-          G.destroyWindow window
-          G.terminate
-          exitSuccess
-          
-mainLoop :: G.Window -> IO ()
-mainLoop w = unless' (G.windowShouldClose w) $ do
-    (width, height) <- G.getFramebufferSize w
-    let ratio = fromIntegral width / fromIntegral height
-    
-    viewport $= (Position 0 0, Size (fromIntegral width) (fromIntegral height))
-    clear [ColorBuffer]
-    
-    matrixMode $= Projection
-    loadIdentity
-    ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
-    matrixMode $= Modelview 0
-    
-    loadIdentity
-    -- this is bad, but keeps the logic of the original example I guess
-    Just t <- G.getTime
-    rotate ((realToFrac t) * 50) $ (Vector3 0 0 1 :: Vector3 GLdouble)
-    
-    renderPrimitive Triangles $ do
-        color  (Color3 1 0 0 :: Color3 GLdouble)
-        vertex (Vertex3 (negate 0.6) (negate 0.4) 0 :: Vertex3 GLdouble)
-        color  (Color3 0 1 0 :: Color3 GLdouble)
-        vertex (Vertex3 0.6 (negate 0.4) 0 :: Vertex3 GLdouble)
-        color  (Color3 0 0 1 :: Color3 GLdouble)
-        vertex (Vertex3 0 0.6 0 :: Vertex3 GLdouble)
-        
-    G.swapBuffers w
-    G.pollEvents
+  if not successfulInit then exitFailure else do
+      mw <- GLFW.createWindow 640 480 "Simple example, haskell style" Nothing Nothing
+      case mw of
+          Nothing -> GLFW.terminate >> exitFailure
+          Just window -> do
+              GLFW.makeContextCurrent mw
+              GLFW.setKeyCallback window (Just keyCallback)
+              mainLoop window
+              GLFW.destroyWindow window
+              GLFW.terminate
+              exitSuccess
+
+mainLoop :: GLFW.Window -> IO ()
+mainLoop w = do
+    close <- GLFW.windowShouldClose w
+    unless close $ do
+        (width, height) <- GLFW.getFramebufferSize w
+        let ratio = fromIntegral width / fromIntegral height
+
+        viewport $= (Position 0 0, Size (fromIntegral width) (fromIntegral height))
+        clear [ColorBuffer]
+
+        matrixMode $= Projection
+        loadIdentity
+        ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
+        matrixMode $= Modelview 0
+
+        loadIdentity
+        -- this is bad, but keeps the logic of the original example I guess
+        Just t <- GLFW.getTime
+        rotate (realToFrac t * 50) (Vector3 0 0 1 :: Vector3 GLdouble)
+
+        renderPrimitive Triangles $ do
+            color  (Color3 1 0 0 :: Color3 GLdouble)
+            vertex (Vertex3 (negate 0.6) (negate 0.4) 0 :: Vertex3 GLdouble)
+            color  (Color3 0 1 0 :: Color3 GLdouble)
+            vertex (Vertex3 0.6 (negate 0.4) 0 :: Vertex3 GLdouble)
+            color  (Color3 0 0 1 :: Color3 GLdouble)
+            vertex (Vertex3 0 0.6 0 :: Vertex3 GLdouble)
+
+    GLFW.swapBuffers w
+    GLFW.pollEvents
     mainLoop w
