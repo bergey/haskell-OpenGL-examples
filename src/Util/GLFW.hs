21df329dbe8039f446df99781fbe4200d5b890f5
Daniel Bergey <bergey@alum.mit.edu>
2013-12-05 04:06:01 +0000

    Simplify Util.GLFW for better tutorials

--- src/Util/GLFW.hs
+++ src/Util/GLFW.hs
@@ -13,27 +13,32 @@ keyCallback :: GLFW.KeyCallback
 keyCallback window key _ action _ = when (key == GLFW.Key'Escape && action == GLFW.KeyState'Pressed) $
   GLFW.setWindowShouldClose window True
 
-mainWith :: ( GLFW.Window -> IO () ) -> IO ()
-mainWith draw = do
+initialize :: IO GLFW.Window
+initialize = do
   GLFW.setErrorCallback (Just errorCallback)
   successfulInit <- GLFW.init
   -- if init failed, we exit the program
   if not successfulInit then exitFailure else do
+      GLFW.windowHint $ GLFW.WindowHint'OpenGLDebugContext True
       mw <- GLFW.createWindow 640 480 "Simple example, haskell style" Nothing Nothing
       case mw of
           Nothing -> GLFW.terminate >> exitFailure
           Just window -> do
               GLFW.makeContextCurrent mw
               GLFW.setKeyCallback window (Just keyCallback)
-              mainLoop draw window
-              GLFW.destroyWindow window
-              GLFW.terminate
-              exitSuccess
+              return window
 
-mainLoop :: ( GLFW.Window -> IO () ) -> GLFW.Window -> IO ()
+cleanup :: GLFW.Window -> IO ()
+cleanup win = do
+    GLFW.destroyWindow win
+    GLFW.terminate
+    exitSuccess
+
+mainLoop :: IO () -> GLFW.Window -> IO ()
 mainLoop draw w = do
     close <- GLFW.windowShouldClose w
-    unless close $ draw w
-    GLFW.swapBuffers w
-    GLFW.pollEvents
-    mainLoop draw w
+    unless close $ do
+                    draw
+                    GLFW.swapBuffers w
+                    GLFW.pollEvents
+                    mainLoop draw w
