37bdef5d0d372c1c09e4805d8c4f5712e9caeee1
Daniel Bergey <bergey@alum.mit.edu>
2013-12-06 15:37:33 +0000

    glfw-modern: add scaling to window aspect ratio

--- glfw/Modern.hs
+++ glfw/Modern.hs
@@ -9,7 +9,7 @@ import qualified Data.ByteString as BS
 -- Import all OpenGL libraries qualified, for pedagogical reasons
 import qualified Graphics.Rendering.OpenGL as GL
 import           Graphics.Rendering.OpenGL (($=))
-import Graphics.Rendering.OpenGL.Raw.Core31 (glUniformMatrix3fv)
+import Graphics.Rendering.OpenGL.Raw.Core31 (glUniformMatrix3fv, glUniformMatrix4fv)
 import qualified Graphics.UI.GLFW as GLFW
 
 -- Local modules
@@ -47,6 +47,7 @@ draw p w = do
 
     GL.currentProgram $= Just p
     GL.UniformLocation tLoc <- GL.get $ GL.uniformLocation p "transform"
+    GL.UniformLocation pLoc <- GL.get $ GL.uniformLocation p "projection"
     GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
     V.unsafeWith vertices $ \ptr -> do
         GL.vertexAttribPointer (GL.AttribLocation 0) $=
@@ -58,6 +59,8 @@ draw p w = do
     t <- GLFW.getTime
     V.unsafeWith (rotateM t) $ \ptr -> do
         glUniformMatrix3fv tLoc 1 1 ptr
+    V.unsafeWith (aspectM ratio) $ \ptr ->
+        glUniformMatrix4fv pLoc 1 1 ptr
     GL.drawArrays GL.Triangles 0 3
     GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Disabled
     GL.vertexAttribArray (GL.AttribLocation 1) $= GL.Disabled
@@ -68,11 +71,18 @@ rotateM Nothing  = V.fromList [ 1, 0, 0
                              , 0, 1, 0
                              , 0, 0, 1
                              ]
-rotateM (Just t') = V.fromList [  cos t,  sin t, 0
-                             , -sin t, -cos t, 0
-                             ,      0,      0, 1
-                             ] where
+rotateM (Just t') = V.fromList [  cos t, -sin t, 0
+                               ,  sin t,  cos t, 0
+                               ,      0,      0, 1
+                               ] where
   t = realToFrac t'
+
+aspectM :: GL.GLfloat -> V.Vector GL.GLfloat
+aspectM r = V.fromList [ 1/r, 0, 0, 0
+                       ,   0, 1, 0, 0
+                       ,   0, 0, 1, 0
+                       ,   0, 0, 0, 1
+                       ]
 
 vsSource, fsSource :: BS.ByteString
 vsSource = BS.intercalate "\n"
@@ -80,9 +90,10 @@ vsSource = BS.intercalate "\n"
            , "attribute vec3 color;"
            , "varying vec3 f_color;"
            , "uniform mat3 transform;"
+           , "uniform mat4 projection;"
            , ""
            , "void main(void) { "
-           , " gl_Position = vec4(transform * coord, 1.0); "
+           , " gl_Position = projection * vec4(transform * coord, 1.0); "
            , " f_color = color;"
            , "}"
            ]
