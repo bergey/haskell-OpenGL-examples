17021b76d14ef2c626d904d69df0041747f43f76
Daniel Bergey <bergey@alum.mit.edu>
2013-12-08 18:12:07 +0000

    Add wiki-t2-vector

---
+++ wikibook/tutorial-02-vbo/Vector.hs
@@ -0,0 +1,63 @@
+-- | GLUtil provides functions to load shaders, so we do not need to
+-- write our own, as the C++ tutorial does.
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+-- General Haskell modules
+import           Control.Applicative
+import System.FilePath ((</>))
+import qualified Data.Vector.Storable as V
+
+-- Import all OpenGL libraries qualified, for pedagogical reasons
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLFW as GLFW
+import           Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.GLUtil as U
+
+-- Local modules
+import qualified Util.GLFW as W
+
+main :: IO ()
+main = do
+    -- GLFW code will be the same in all variants
+    win <- W.initialize "My First Triangle"
+    prog <- initResources
+    W.mainLoop (draw prog win) win
+    W.cleanup win
+
+initResources :: IO Program
+initResources = do
+    -- compile vertex shader
+    vs <- U.loadShader GL.VertexShader $ shaderPath </> "triangle.v.glsl"
+    fs <- U.loadShader GL.FragmentShader $ shaderPath </> "triangle.f.glsl"
+    p <- U.linkShaderProgram [vs, fs]
+    Program p <$> GL.get (GL.attribLocation p "coord2d")
+
+draw :: Program -> GLFW.Window -> IO ()
+draw (Program program attrib) win = do
+    GL.clearColor $= GL.Color4 1 1 1 1
+    GL.clear [GL.ColorBuffer]
+    -- In C++ example GLUT handles this?
+    (width, height) <- GLFW.getFramebufferSize win
+    GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral width) (fromIntegral height))
+
+    GL.currentProgram $= Just program
+    GL.vertexAttribArray attrib $= GL.Enabled
+    V.unsafeWith vertices $ \ptr ->
+        GL.vertexAttribPointer attrib $=
+          (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float 0 ptr)
+    GL.drawArrays GL.Triangles 0 3 -- 3 is the number of vertices
+    GL.vertexAttribArray attrib $= GL.Disabled
+
+-- | Represents the shader program and its input parameter
+data Program = Program GL.Program GL.AttribLocation
+
+shaderPath :: FilePath
+shaderPath = "wikibook" </> "tutorial-02-vbo"
+
+vertices :: V.Vector Float
+vertices = V.fromList [  0.0,  0.8
+                      , -0.8, -0.8
+                      ,  0.8, -0.8
+                      ]
