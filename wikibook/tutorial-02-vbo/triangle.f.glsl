bcc544bd9090de2014926d355a54981e68a42604
Daniel Bergey <bergey@alum.mit.edu>
2013-12-08 18:46:02 +0000

    Actually use a fragment shader that works

--- wikibook/tutorial-02-vbo/triangle.f.glsl
+++ wikibook/tutorial-02-vbo/triangle.f.glsl
@@ -1,6 +1,3 @@
 void main(void) {
-  gl_FragColor[0] = 0.0;
-  gl_FragColor[1] = 0.0;
-  gl_FragColor[2] = 1.0;
-  gl_FragColor[3] = floor(mod(gl_FragCoord.y, 2.0));
+  gl_FragColor = vec4(0, 0, 1, 1);
 }
