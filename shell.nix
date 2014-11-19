46a19ad4e55c55b53021c3ae80fe9079165f57e9
Daniel Bergey <bergey@alum.mit.edu>
2014-11-19 20:10:42 +0000

    allow linear-1.15.*
    update shell.nix to test latest packages

--- shell.nix
+++ shell.nix
@@ -1,15 +1,14 @@
 { pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages }:
 
 let 
-  tmpHaskellPkgs= haskellPackages.override {
-        extension = self: super: {
-          GLUtil = self.callPackage /home/bergey/code/nixHaskellVersioned/GLUtil/0.8.2.nix {};
-          vinylGl = self.callPackage /home/bergey/code/nixHaskellVersioned/vinyl-gl/0.2.1.nix {};
+  hs = haskellPackages.override {
+        extension = self: super: rec {
+          hsPkg = pkg: version: self.callPackage "/home/bergey/code/nixHaskellVersioned/${pkg}/${version}.nix" {};
+          vinylGl = hsPkg "vinyl-gl" "0.2.1";
+          linear = hsPkg "linear" "1.15.1";
           currentPkg = self.callPackage ./. {};
       };
     };
-  in let
-     haskellPackages = tmpHaskellPkgs;
-     in pkgs.lib.overrideDerivation haskellPackages.currentPkg (attrs: {
-       buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
+    in pkgs.lib.overrideDerivation hs.currentPkg (attrs: {
+        buildInputs = [hs.cabalInstall ] ++ attrs.buildInputs;
  })
